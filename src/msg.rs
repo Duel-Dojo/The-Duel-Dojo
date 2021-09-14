fd90b2a2c6e8688478cc5b7ce2781138a63e5a37
Vlad <vladjdk@gmail.com>
2021-09-13 23:17:16 -0400

    cargo clippy fix

--- src/msg.rs
+++ src/msg.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::{Addr, Api, Coin, StdResult};
 
 use cw20::{Balance, Cw20Coin, Cw20ReceiveMsg};
+use crate::state::GenericBalance;
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 pub struct InstantiateMsg {
@@ -13,26 +14,6 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Create(CreateMsg),
-    /// Adds all sent native tokens to the contract
-    TopUp {
-        id: String,
-    },
-    /// Approve sends all tokens to the recipient.
-    /// Only the arbiter can do this
-    Approve {
-        /// id is a human-readable name for the escrow from create
-        id: String,
-    },
-    /// Refund returns all remaining tokens to the original sender,
-    /// The arbiter can do this any time, or anyone can do this after a timeout
-    Refund {
-        /// id is a human-readable name for the escrow from create
-        id: String,
-    },
-    /// This accepts a properly-encoded ReceiveMsg from a cw20 contract
-    Receive(Cw20ReceiveMsg),
-
     //DUEL DOJO NEW FUNCTIONS
     // Creates an instance of the escrow and adds funds from User 1.
     // Creates an escrow ID that can later be referenced. Sets User 1
@@ -59,16 +40,6 @@ pub enum ExecuteMsg {
     SendFunds {
         wager_id: String,
         winner_address: Addr,
-    },
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum ReceiveMsg {
-    Create(CreateMsg),
-    /// Adds all sent native tokens to the contract
-    TopUp {
-        id: String,
     },
 }
 
@@ -114,40 +85,39 @@ pub fn is_valid_name(name: &str) -> bool {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    /// Show all open escrows. Return type is ListResponse.
-    List {},
-    /// Returns the details of the named escrow, error if not created
-    /// Return type: DetailsResponse.
-    Details { id: String },
+    //TODO: query messages
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 pub struct ListResponse {
     /// list all registered ids
-    pub escrows: Vec<String>,
+    pub wagers: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 pub struct DetailsResponse {
     /// id of this escrow
     pub id: String,
-    /// arbiter can decide to approve or refund the escrow
-    pub arbiter: String,
-    /// if approved, funds go to the recipient
-    pub recipient: String,
-    /// if refunded, funds go to the source
-    pub source: String,
-    /// When end height set and block height exceeds this value, the escrow is expired.
+    /// arbiter can decide to approve or refund the escrow, this is typically the game address
+    pub arbiter: Addr,
+    /// creator of contract
+    pub user1: Addr,
+    /// Player 2 that joined the contract
+    pub user2: Addr,
+    /// Player 1 Balance in Native and Cw20 tokens
+    /// When end height set and block height exceeds this value, the wager is expired.
     /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_height: Option<u64>,
-    /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
-    /// block time exceeds this value, the escrow is expired.
-    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_time: Option<u64>,
-    /// Balance in native tokens
-    pub native_balance: Vec<Coin>,
-    /// Balance in cw20 tokens
-    pub cw20_balance: Vec<Cw20Coin>,
-    /// Whitelisted cw20 tokens
-    pub cw20_whitelist: Vec<String>,
+    // pub end_height: Option<u64>, // TODO: FIX END TIME AND END HEIGHT
+    // FIX END TIME -> Wager expires 200 blocks AFTER wager is confirmed (20 mins, ~6 secs per block)
+    // /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
+    // /// block time exceeds this value, the escrow is expired.
+    // /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    // pub end_time: Option<u64>,
+    /// Balance in Native and Cw20 tokens
+    pub user1_balance: GenericBalance,
+    /// Player 2 Balance in Native and Cw20 tokens
+    pub user2_balance: GenericBalance,
+    // /// All possible contracts that we accept tokens from
+    // pub cw20_whitelist: Vec<Addr>, // TODO: WHITELIST?
+    // WHITELIST? -> Only DUEL tokens available for wager
 }
--- src/state.rs
+++ src/state.rs
@@ -48,58 +48,7 @@ impl GenericBalance {
     }
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct Escrow {
-    /// arbiter can decide to approve or refund the escrow
-    pub arbiter: Addr,
-    /// if approved, funds go to the recipient
-    pub recipient: Addr,
-    /// if refunded, funds go to the source
-    pub source: Addr,
-    /// When end height set and block height exceeds this value, the escrow is expired.
-    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_height: Option<u64>,
-    /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
-    /// block time exceeds this value, the escrow is expired.
-    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
-    pub end_time: Option<u64>,
-    /// Balance in Native and Cw20 tokens
-    pub balance: GenericBalance,
-    /// All possible contracts that we accept tokens from
-    pub cw20_whitelist: Vec<Addr>,
-}
 
-impl Escrow {
-    pub fn is_expired(&self, env: &Env) -> bool {
-        if let Some(end_height) = self.end_height {
-            if env.block.height > end_height {
-                return true;
-            }
-        }
-
-        if let Some(end_time) = self.end_time {
-            if env.block.time > Timestamp::from_seconds(end_time) {
-                return true;
-            }
-        }
-
-        false
-    }
-
-    pub fn human_whitelist(&self) -> Vec<String> {
-        self.cw20_whitelist.iter().map(|a| a.to_string()).collect()
-    }
-}
-
-pub const ESCROWS: Map<&str, Escrow> = Map::new("escrow");
-
-/// This returns the list of ids for all registered escrows
-pub fn all_escrow_ids(storage: &dyn Storage) -> StdResult<Vec<String>> {
-    ESCROWS
-        .keys(storage, None, None, Order::Ascending)
-        .map(|k| String::from_utf8(k).map_err(|_| StdError::invalid_utf8("parsing escrow key")))
-        .collect()
-}
 
 // DUEL DOJO CODE BELOW TODO: remove above
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
@@ -160,49 +109,4 @@ pub fn all_wager_ids(storage: &dyn Storage) -> StdResult<Vec<String>> {
         .keys(storage, None, None, Order::Ascending)
         .map(|k| String::from_utf8(k).map_err(|_| StdError::invalid_utf8("parsing escrow key")))
         .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use cosmwasm_std::testing::MockStorage;
-
-    #[test]
-    fn no_escrow_ids() {
-        let storage = MockStorage::new();
-        let ids = all_escrow_ids(&storage).unwrap();
-        assert_eq!(0, ids.len());
-    }
-
-    fn dummy_escrow() -> Escrow {
-        Escrow {
-            arbiter: Addr::unchecked("arb"),
-            recipient: Addr::unchecked("recip"),
-            source: Addr::unchecked("source"),
-            end_height: None,
-            end_time: None,
-            balance: Default::default(),
-            cw20_whitelist: vec![],
-        }
-    }
-
-    #[test]
-    fn all_escrow_ids_in_order() {
-        let mut storage = MockStorage::new();
-        ESCROWS
-            .save(&mut storage, &"lazy", &dummy_escrow())
-            .unwrap();
-        ESCROWS
-            .save(&mut storage, &"assign", &dummy_escrow())
-            .unwrap();
-        ESCROWS.save(&mut storage, &"zen", &dummy_escrow()).unwrap();
-
-        let ids = all_escrow_ids(&storage).unwrap();
-        assert_eq!(3, ids.len());
-        assert_eq!(
-            vec!["assign".to_string(), "lazy".to_string(), "zen".to_string()],
-            ids
-        )
-    }
 }
